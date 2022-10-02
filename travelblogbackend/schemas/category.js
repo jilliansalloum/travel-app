902f2e023ca14f049a70fce1b35177f200a0ce31
Jillian Salloum <salloum1@msu.edu>
2022-10-02 18:11:14 -0400

    first!

---
+++ travelblogbackend/schemas/category.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'category',
+    title: 'Category',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            tiles: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+        }
+    ]
+}
